984b6dd6095732bdd90ce56e752cac9fc5d519a3
Oskar van Velden <oskar@rakso.nl>
2018-09-03 13:52:01 +0200

    Change github-repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Netlify + VuePress',
   themeConfig: {
     docsDir: 'docs',
-    repo: 'andreliem/vuepress-netlify-cms',
+    repo: 'skarnl/vuepress',
     sidebar: [
       '/',
       '/welcome',
